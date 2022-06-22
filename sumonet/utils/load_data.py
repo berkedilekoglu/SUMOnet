00f388e8c418a209c1c16023d2659eacb8e34c75
berkedilekoglu <berkedilekoglu@gmail.com>
2022-06-22 20:14:51 +0300

    Data class is added

---
+++ sumonet/utils/load_data.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import random
+
+modulePath = str(Path(__file__).parent.parent.resolve())
+dataPath = modulePath + "/data/"
+
+def get_train_data():
+
+        trainDataPath = dataPath + "train/"
+
+        posTrainDataPath = trainDataPath + "Sumoylation_pos_Train.fasta"
+        negTrainDataPath = trainDataPath + "Sumoylation_neg_Train.fasta"
+
+
+        return posTrainDataPath, negTrainDataPath
+
+def get_test_data():
+
+        testDataPath = dataPath + "test/"
+
+        posTestDataPath = testDataPath + "Sumoylation_pos_Test.fasta"
+        negTestDataPath = testDataPath + "Sumoylation_neg_Test.fasta"
+
+
+        return posTestDataPath, negTestDataPath
+
+class Data:
+
+        def __init__(self):
+               
+                
+                self.posTrainDataPath, self.negTrainDataPath = get_train_data()
+                self.posTestDataPath, self.negTestDataPath = get_test_data()
+
+
+        def get_new_data(self,filePath):
+        
+                with open(filePath) as f:
+                        
+                        my_array = f.read().splitlines()
+        
+                return my_array[1::2]
+
+        def all_data(self):
+
+                posTrainData = self.get_new_data(self.posTrainDataPath)
+                negTrainData = self.get_new_data(self.negTrainDataPath)
+
+                posTestData = self.get_new_data(self.posTestDataPath)
+                negTestData = self.get_new_data(self.negTestDataPath)
+
+                return [posTrainData, negTrainData, posTestData, negTestData]
+
+        def randomly_sample(self, data, ratio):
+
+                return random.sample(data,k=int((ratio/2)*len(data)))
+
+
+        def sample_data(self, ratio=0.4):
+
+                
+
+                sampledPosTrainData, sampledNegTrainData, sampledPosTestData, sampledNegTestData = list(map(self.randomly_sample,self.all_data(),[ratio]*len(self.all_data())))
+
+                X_train = len(sampledPosTrainData) + sampledNegTrainData
+                X_test = len(sampledPosTestData) + sampledNegTestData
+
+                y_train = [1]*len(sampledPosTrainData) + [0]*sampledNegTrainData
+                y_test =  [1]*len(sampledPosTestData) + [0]*sampledNegTestData
+
+
+                return X_train, y_train, X_test, y_test
