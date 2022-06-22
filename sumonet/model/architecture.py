f73ddf90fcd09936c840d1081fd0fb75ee093079
berkedilekoglu <berkedilekoglu@gmail.com>
2022-06-22 21:21:06 +0300

    SUMOnet3 architecture is added

--- sumonet/model/architecture.py
+++ sumonet/model/architecture.py
@@ -1,21 +1,23 @@
-from tensorflow.keras import layers
-from tensorflow.keras.regularizers import l2
 
-class SUMOnet(layers.Layer):
+from tensorflow.keras import layers, Model, losses, regularizers
+
+class Architecture(Model):
 
     def __init__(self):
         
-        super(SUMOnet,self).__init__()
+        super(Architecture,self).__init__()
 
         self.cnn = layers.Conv1D(128,2,padding='valid',activation='relu',kernel_initializer='he_normal',strides=1)
         self.bigru = layers.Bidirectional(layers.GRU(32, dropout=0.4, recurrent_dropout=0,return_sequences=True))
         self.pool = layers.GlobalAveragePooling1D()
-        self.dense64 = layers.Dense(64,kernel_initializer='he_normal',activity_regularizer= l2(1e-4))
+        self.dense64 = layers.Dense(64,kernel_initializer='he_normal',activity_regularizer= regularizers.l2(1e-4))
         self.dropout = layers.Dropout(0.4)
         self.relu = layers.Activation('relu')
-        self.dense128 = layers.Dense(128,kernel_initializer='he_normal',activity_regularizer= l2(1e-4))
+        self.dense128 = layers.Dense(128,kernel_initializer='he_normal',activity_regularizer= regularizers.l2(1e-4))
         self.dense2 = layers.Dense(2, kernel_initializer='he_normal')
         self.softmax = layers.Activation('softmax')
+
+        
 
 
 
@@ -36,5 +38,5 @@ class SUMOnet(layers.Layer):
         x = self.relu(x)
         x = self.dense2(x)
         x = self.softmax(x)
-
+        
         return x
