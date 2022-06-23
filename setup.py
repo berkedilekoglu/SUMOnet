1a0bc412377e5657ced530fd6dde2be15ba89d50
berkedilekoglu <berkedilekoglu@gmail.com>
2022-06-24 00:34:08 +0300

    license update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='sumonet',
     version='0.1',
-    license='MIT',
+    license='Academic Free License ("AFL") v. 3.0',
     author="Berke Dilekoglu",
     author_email='dilekogluberke@gmail.com',
     packages=find_packages('sumonet'),
